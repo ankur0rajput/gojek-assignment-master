8cca011020663d94bb4c1188809229bcdf56a922
ankur0rajput <ankur0rajput@gmail.com>
2024-06-10 09:33:18 +0530

    added accuracy metrics in evaluate func

--- src/models/classifier.py
+++ src/models/classifier.py
@@ -4,6 +4,7 @@ from typing import Dict, List
 import numpy as np
 import pandas as pd
 from sklearn.base import BaseEstimator
+from sklearn.metrics import accuracy_score
 
 
 class Classifier(ABC):
@@ -32,10 +33,14 @@ class SklearnClassifier(Classifier):
         self.clf.fit(df_train[self.features].values, df_train[self.target].values)
 
     def evaluate(self, df_test: pd.DataFrame):
+        prediction = self.clf.predict(df_test[self.features].values)
+        metric_name = 'accuracy_score'
+        metric_score = accuracy_score(df_test[self.target].values, prediction)
+        return {metric_name: metric_score}
+        '''
         raise NotImplementedError(
             f"You're almost there! Identify an appropriate evaluation metric for your model and implement it here. "
-            f"The expected output is a dictionary of the following schema: {{metric_name: metric_score}}"
-        )
-
+            f"The expected output is a dictionary of the follow
+        '''    
     def predict(self, df: pd.DataFrame):
         return self.clf.predict_proba(df[self.features].values)[:, 1]
